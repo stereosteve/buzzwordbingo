90733ad9c6763beef06965436f84d4c214cf8477
Steve Perkins <stereosteve@gmail.com>
2012-07-01 10:22:03 -0400

    check user input

--- README.md
+++ README.md
@@ -2,5 +2,3 @@
 
 A multiplayer bingo app.
 
-
-In order to leverage social networks and maximize Return on Investment, I have create a mobile cloud computing platform for the gamification of buzzword bingo.  This location-based HTML5 webapp leverages node.js to provide an immersive realtime multiplayer game experience.  At the height of the bubble era, this type of app is exactly what the market wants to realize the profit-potential
--- server.js
+++ server.js
@@ -9,10 +9,10 @@ app.use(express.static(__dirname + '/app'))
 
 var io = require('socket.io').listen(app)
 
-io.configure(function() {
-  io.set("transports", ["xhr-polling"])
-  io.set("polling duration", 10)
-})
+//io.configure(function() {
+  //io.set("transports", ["xhr-polling"])
+  //io.set("polling duration", 10)
+//})
 
 
 
@@ -28,9 +28,6 @@ var world = {
   events: [],
 }
 
-world.findGame = function(id) {
-  return world.games[id]
-}
 
 /**
  * Create some test games
@@ -97,52 +94,59 @@ io.sockets.on('connection', function(socket) {
   // setNick
 
   socket.on('joinGame', function(gameId, callback) {
-    var game = world.games[gameId]
     if (me.boards[gameId]) {
       callback(me.boards[gameId])
       return
     }
-    var board = new Board({
-      gameId: gameId,
-      userId: me.id,
-      vocab: game.vocab
-    })
-    me.boards[gameId] = board
-    game.users[me.id] = me
-    updateWorld()
-    callback(board)
+    else if (world.games[gameId]) {
+      var game = world.games[gameId]
+      var board = new Board({
+        gameId: gameId,
+        userId: me.id,
+        vocab: game.vocab
+      })
+      me.boards[gameId] = board
+      game.users[me.id] = me
+      updateWorld()
+      callback(board)
+    }
+    else {
+      console.log("game not found " + gameId)
+    }
   })
 
   socket.on('markCell', function(gameId, cell) {
-    var game = world.findGame(gameId)
+    var game = world.games[gameId]
     var board = me.boards[gameId]
-    board.markCell(cell.num)
 
-    var ev = {
-      msg: me.nick + " marked " + cell.word,
-      date: new Date()
-    }
-    game.events.push(ev)
-    updateWorld(ev)
+    if (game && board) {
+      board.markCell(cell.num)
 
-    if (board.isWinner()) {
-      var win = me.nick + " got BINGO!"
-      game.events.push(win)
-      updateWorld(win)
-      socket.emit('winner', me)
-      socket.broadcast.emit('winner', me)
+      var ev = {
+        msg: me.nick + " marked " + cell.word,
+        date: new Date()
+      }
+      game.events.push(ev)
+      updateWorld(ev)
+
+      if (board.isWinner()) {
+        var win = me.nick + " got BINGO!"
+        game.events.push(win)
+        updateWorld(win)
+        socket.emit('winner', me)
+        socket.broadcast.emit('winner', me)
+      }
     }
   })
 
   socket.on('unmarkCell', function(gameId, cell) {
     var board = me.boards[gameId]
-    board.unmarkCell(cell.num)
+    if (board) board.unmarkCell(cell.num)
   })
 
 
   socket.on('updateNick', function(nick) {
-    console.log(nick)
-    me.nick = nick
+    if (nick && nick.length > 0) me.nick = nick
   })
 
 
